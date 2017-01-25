0c5be73b3032946cc1867e0c918f2c5a048b9bb8
Grzesiek Pluta <pluta7@gmail.com>
2017-01-26 00:04:39 +0100

    Add sample index.php with inlining

---
+++ src/public/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>TITLE</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <meta name="description" content="..."/>
+  <meta property="og:title" content="..."/>
+  <meta property="og:site_name" content="..."/>
+  <meta property="og:description" content="..."/>
+  <meta property="og:image" content="..."/>
+  <meta property="og:url" content="..."/>
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+<div style="display: none;">
+  @@include('../svg-sprite.svg')
+</div>
+
+<?php
+echo "<h1>Welcome!</h1>";
+?>
+
+<script src="/js/scripts.js"></script>
+</body>
+</html>
